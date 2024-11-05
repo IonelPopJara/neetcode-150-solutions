3f1b456214b42594c7e0ef10800a518812aeb9a2
Ionel Pop <ionelalexandru.pop@gmail.com>
2024-11-06 00:49:32 +0100

    I hate hash tables

--- 1-arrays-and-hashing/1.5-top-k-frequent-elements.c
+++ 1-arrays-and-hashing/1.5-top-k-frequent-elements.c
@@ -1,90 +1,145 @@
-
 // @leet start
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_VALUE 10000
-#define HASHMAP_SIZE (MAX_VALUE * 2)
+#define TABLE_SIZE 17
 
-#define TABLE_SIZE 10007
-
-typedef struct node {
+typedef struct entry {
+    int key;
     int val;
-    struct node* next;
-} Node;
+    struct entry* next;
+} Entry;
 
-int hash(int val) {
-    return val + MAX_VALUE;
+typedef struct hashTable {
+    Entry** entries;
+} HashTable;
+
+HashTable* initHashTable() {
+    HashTable* table = (HashTable*)calloc(1, sizeof(HashTable));
+
+    table->entries = (Entry**)calloc(TABLE_SIZE, sizeof(Entry*));
+
+    return table;
 }
 
-// This is kinda stupid but it's a work around for not wanting to do a hashmap
-int dehash(int val) {
-    return val - MAX_VALUE;
+int hash(int key) {
+    return abs(key) % TABLE_SIZE;
 }
 
-int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
-    /*
-     * Min and Max values for nums[i] => [-10000, 10000]
-     * -10000 -map-> 0 | 0 -map-> 10000 | 10000 -map->20000
-     */
-
-    // Create a hashmap to map the numbers to their count
-    // For example, for the input array [1, 1, 1, 2, 2, 3],
-    // we get {MAX_VALUE + 1: 3, MAX_VALUE + 2: 2, MAX_VALUE + 3: 1}.
-    int countMap[MAX_VALUE * 2] = { 0 }; // Array
-
-    // Create an array to store the count of each number, where the index
-    // of the array represent the count.
-    // For example, for the countMap {MAX_VALUE + 1: 3, MAX_VALUE + 2: 2, MAX_VALUE + 3: 1},
-    // we get [_, 3, 2, 1, _, _, _, _].
-    // Where index 0 is not used since there is no element with count 0.
-    int* countArray = (int*)malloc(sizeof(int) * (numsSize + 1));
-    for (int i = 0; i < numsSize + 1; i++) {
-        countArray[i] = -MAX_VALUE - 1;
-    }
-
-    // Iterate through the input array
-    for (int i = 0; i < numsSize; i++) {
-        // Update the count in countMap
-        int hashed = hash(nums[i]);
-        countMap[hashed] += 1;
-    }
-
-    // Iterate through the count map [0, 20000] where it maps to [-10000, 10000]
-    for (int i = 0; i < HASHMAP_SIZE; i++) {
-        // Update the count array
-        int count = countMap[i];
-        if (count > 0) {
-            countArray[count] = dehash(i);
-        }
-    }
-
-    int* result = (int*)malloc(sizeof(int) * k);
-    int resultCount = k;
-    *returnSize = k;
-
-    printf("\n");
-    for (int i = numsSize; i > 0; i--) {
-        printf("countArray[%d]: %d\n", i, countArray[i]);
-    }
-    printf("\n");
-
-    // Iterate in revers order through the count array
-    // Most frequent numbers will be last since they have higher indexes
-    for (int i = numsSize; i > 0; i--) {
-        printf("i: %d\n", i);
-        if (resultCount == 0) {
+void insert(HashTable* table, int key, int val) {
+    int hashed = hash(key);
+    Entry* current = table->entries[hashed];
+    while (current) {
+        if (current->key == key) {
             break;
         }
-        // FIXME: Change this
-        if (countArray[i] > -MAX_VALUE - 1) {
-            printf("CountArray[%d]: %d\n", i, countArray[i]);
-            result[resultCount - 1] = countArray[i];
-            resultCount--;
+        current = current->next;
+    }
+
+    if (current) {
+        // If we found an already existing key, we update it
+        current->val = val;
+    } else {
+        // Otherwise, we insert a new node (separate chaining)
+        Entry* newEntry = (Entry*)calloc(1, sizeof(Entry));
+        newEntry->key = key;
+        newEntry->val = val;
+        newEntry->next = table->entries[hashed];
+        table->entries[hashed] = newEntry;
+    }
+}
+
+Entry* get(HashTable* table, int key) {
+    int hashed = hash(key);
+    Entry* current = table->entries[hashed];
+    while (current) {
+        if (current->key == key) {
+            return current;
+        }
+
+        current = current->next;
+    }
+
+    return NULL;
+}
+
+void printHashTable(HashTable* table) {
+    printf("Printing hash table...\n");
+    printf("{");
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        Entry* current = table->entries[i];
+        while (current) {
+            printf("%d: %d, ", current->key, current->val);
+            current = current->next;
+
+        }
+    }
+    printf("}\n");
+}
+
+
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
+
+    // Input Array: [min, max] | [-10000, 10000]
+    //  [1, 1, 2, 3, 3]
+    //  map to -> {1: 2, 2: 1, 3: 2} (dictionary)
+    HashTable* countTable = initHashTable();
+    for (int i = 0; i < numsSize; i++) {
+        Entry* current = get(countTable, nums[i]);
+
+        if (current) {
+            // If it already exists, increment the value (count)
+            insert(countTable, nums[i], current->val + 1);
+        } else {
+            // Otherwise create a new entry
+            insert(countTable, nums[i], 1);
+        }
+    }
+
+    /*printHashTable(countTable);*/
+
+    // Map {1: 2, 2: 1, 3: 2} to
+    // [X, 2, [1, 3], _, _, _] (1-index list of numsSize + 1. Index 0 is useless)
+    int** freqGroup = (int**)calloc(numsSize + 1, sizeof(int*));
+    int* freqGroupSizes = (int*)calloc(numsSize + 1, sizeof(int));
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        Entry* current = countTable->entries[i];
+        // NOTE: For some reason this while was an if and it took me 5 minutes to realize
+        while (current) {
+            int freq = current->val;
+            freqGroupSizes[freq]++; // If I have two elements with the same freq, [1, 2] -> freq = 1, update freqGroupSizes[1]
+            freqGroup[freq] = (int*)realloc(freqGroup[freq], freqGroupSizes[freq] * sizeof(int));
+            // Add it to the list of lists
+            freqGroup[freq][freqGroupSizes[freq] - 1] = current->key;
+            current = current->next;
+        }
+    }
+
+    // Print list of lists
+    /*printf("\nPrinting frequency list...\n");*/
+    /*for (int i = 0; i < numsSize; i++) {*/
+    /*    printf("    freqGroup[%d]:\n", i);*/
+    /*    for (int j = 0; j < freqGroupSizes[i]; j++) {*/
+    /*        printf("        [%d]: %d\n", j, freqGroup[i][j]);*/
+    /*    }*/
+    /*}*/
+    /*printf("\n");*/
+
+    *returnSize = k;
+
+    int* result = (int*)calloc(k, sizeof(int));
+    int resultIndex = 0;
+
+    for (int i = numsSize; i > 0 && resultIndex < k; i--) {
+        // We have a frequency
+        if (freqGroupSizes[i] > 0) {
+            for (int j = 0; j < freqGroupSizes[i] && resultIndex < k; j++) {
+                result[resultIndex] = freqGroup[i][j];
+                resultIndex++;
+            }
         }
     }
 
